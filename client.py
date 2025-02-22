f2e922b33a2b08941d0b6f670b8680d1b98a603f
nickdu088 <26584779+nickdu088@users.noreply.github.com>
2025-02-22 21:21:10 +1100

    Add files via upload

--- client.py
+++ client.py
@@ -1,116 +1,248 @@
-import socket
-import sys
+#!/usr/bin/env python
 import json
+import socket
+import socketserver
+import time
 import requests
+from uuid import uuid4
+import threading
+import argparse
+import logging
 
-def createHost(url: str, type: str, port: int):
-    try:
-        if type == "tcp":
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        elif type == "udp":
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(process)s] [%(levelname)s] %(message)s")
+logg = logging.getLogger(__name__)
+
+BUFFER = 1024 * 50
+
+class TunnelPersistentConnection:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+    
+    def __init__(self, connection_id = None, port : int = -1):
+        self.id = connection_id
+        self.port = port
+        self.session = requests.Session()
+        self.sender = None
+
+    def get_settings(self):
+        return json.dumps({"channel": self.id, "port": self.port})
+
+    def get_channel_url(self):
+        if self.id:
+            return self.get_url() + self.id
         else:
-            print(f"Unsupported socket type: {type}")
-            sys.exit(1)
-        sock.connect(("192.168.0.110", port))
+            return self.get_url()
+    
+    def get_url(self):
+        return "http://192.168.0.67:9999/"
 
-        print(f"Connected to localhost on port {port} with type {type}")
+    def create(self):
+        logg.info("Creating connection to remote tunnel")
+        headers = {"Content-Type": "application/json", "Accept": "text/plain"}
+        try:
+            data = self.get_settings()
+            logg.info("Creating connection with settings: %s" % data)
+            response = self.session.post(url=self.get_url(), data=self.get_settings(), headers=headers)
+            if response.status_code == 200:
+                settins = response.json()
+                self.id = settins["channel"]
+                self.port = int(settins["port"])
+                logg.info('Successfully create connection : %s' % self.get_settings())
+                return True
+            logg.info('Fail to establish connection: status %s because %s' % (response.status_code, response.reason))
+            return False 
+        except Exception as ex:
+            logg.error("Error Creating Connection: %s" % ex)
+            return False
 
+    def forward(self, data):
+        headers = {"Content-Type": "application/octet-stream", "Accept": "*/*"}
+        response = self.session.put(url= self.get_channel_url(), data=data, headers=headers)
+        if (response.status_code == 200):
+            logg.info("Data forwarded to remote tunnel")
+            return True
+        else:
+            logg.info("Fail to forward data to remote tunnel error: %s" % response.reason)
+            return False
+
+
+    def receive(self):
+        response = self.session.get(self.get_channel_url())
+        data = response.content
+        if response.status_code == 200:
+            return data
+        else: 
+            return None
+
+    def close(self):
+        logg.info("Close connection to target at remote tunnel")
+        self.session.delete(self.get_channel_url())
+        self.session.close()
+
+    def run(self):
+        sender = None
         while True:
             try:
-                data = sock.recv(1024)
-                if not data:
-                    response = requests.get(url)
-                    socket.sendall(response.content)
-                print(f"Received data: {data.decode('utf-8')}")
-                requests.post(url, data)
-            except Exception as e:
-                print(f"Error receiving data: {e}")
+                response = self.session.get(self.get_channel_url())
+                if response.status_code == 200:
+                    data = response.content
+                    if data:
+                        logg.info("Data received from tunnel: %s" % data)
+                        if sender == None:
+                            logg.info("Creating sender")
+                            sender = TCPProxyClient("localhost", self.port, self)
+                            sender.start()
+                        sender.send(data)
+                    else:
+                        # logg.info("No data received")
+                        time.sleep(1)
+                else:
+                    # logg.info("Fail to get data from remote tunnel")
+                    time.sleep(1)
+            except Exception as ex:
+                logg.error("Error Receiving Data: %s" % ex)
                 break
 
-        sock.close()
-    except Exception as e:
-        print(f"Failed to connect to localhost on port {port}: {e}")
-    else:
-        print("Type and port parameters are required to create a socket connection.")
+class ReceiveThread(threading.Thread):
 
-def createClient(url: str, type: str, port: int):
-    try:
-        if type == "tcp":
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.bind(("127.0.0.1", port))
-            sock.listen(1)
-            print(f"TCP server listening on port {port}")
-            conn, addr = sock.accept()
-            print(f"Connection from {addr}")
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print(f"Received data: {data.decode('utf-8')}")
-                response = requests.get(url)
-                conn.sendall(response.content)
-            conn.close()
-        elif type == "udp":
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.bind(("127.0.0.1", port))
-            print(f"UDP server listening on port {port}")
-            while True:
-                data, addr = sock.recvfrom(1024)
-                if not data:
-                    break
-                print(f"Received data from {addr}: {data.decode('utf-8')}")
-                response = requests.get(url)
-                sock.sendto(response.content, addr)
-        else:
-            print(f"Unsupported socket type: {type}")
-            sys.exit(1)
-    except Exception as e:
-        print(f"Failed to create server on port {port}: {e}")
-    finally:
-        sock.close()
+    """
+    Thread to receive data from remote host
+    """
 
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: python client.py -t <type> -p <port> | -c <channel>")
-        sys.exit(1)
+    def __init__(self, client, connection):
+        threading.Thread.__init__(self, name="Receive-Thread")
+        self.client = client
+        self.conn = connection
+        self._stop = threading.Event()
+
+    def run(self):
+        while not self.stopped():
+            try:
+                logg.info("Retreiving data from remote tunneld")
+                data = self.conn.receive()
+                if data:
+                    logg.info("Data received ... %s" % data)
+                    self.client.sendall(data)
+                else:
+                    # logg.info("No data received")
+                    # sleep for sometime before trying to get data again
+                    time.sleep(1)
+            except Exception as ex:
+                logg.error("Error: %s" % ex)
+                break
+
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.is_set()
+
+class TCPProxyHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        # self.rfile is a file-like object created by the handler.
+        # We can now use e.g. readline() instead of raw recv() calls.
+        # We limit ourselves to 10000 bytes to avoid abuse by the sender.
+        while True:
+            try:
+                data = self.request.recv(BUFFER)
+                logg.info("Getting data from client to send")
+                if data == b'':
+                    logg.info("Client's socket connection broken")
+                    break
+                logg.info("Sending data ... %s " % data)
+                self.server.forward_request(data)
+            except socket.timeout:
+                logg.info("time out")
+            except Exception as ex:
+                logg.error("Error: %s" % ex)
+                break
+
+class TCPProxyServer(socketserver.TCPServer):
+    def __init__(self, server_address, RequestHandlerClass, connection, bind_and_activate=True):
+        super().__init__(server_address, RequestHandlerClass, bind_and_activate)
+        self.connection = connection
+        self.handle_request
+
+    def process_request(self, request, client_address):
+        # if not self.connection.create():
+        #     logg.error("Fail to establish connection")
+        #     self.finish()
+        self.receiver = ReceiveThread(request, self.connection)
+        self.receiver.start()
+        return super().process_request(request, client_address)
+        
+
+    def forward_request(self, data):
+        self.connection.forward(data)
+
+    def shutdown_request(self, request):
+        if self.receiver.is_alive():
+            self.receiver.stop()
+            # self.receiver.join()
+        return super().shutdown_request(request)
+
+class TCPProxyClient(threading.Thread):
+    def __init__(self, host, port, connection):
+        threading.Thread.__init__(self, name="Client-Thread")
+        self.host = host
+        self.port = port
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection = connection
+        # open socket connection to remote server
+        self.s.connect_ex((self.host, self.port))
+        # use non-blocking socket
+        # self.s.setblocking(0)
+        self._stop = threading.Event()
     
-    parameters = sys.argv
-    settings = {}
-    for i in range(1, len(parameters)):
-        parameter = parameters[i]
-        if parameters[i] == "-t":
-            if i + 1 >= len(parameters):
-                print("Usage: python test.py -t <type>")
-                sys.exit(1)
-            settings["type"] = parameters[i + 1].lower()
-        elif parameter == "-c":
-            if i + 1 >= len(parameters):
-                print("Usage: python test.py -c <channel>")
-                sys.exit(1)
-            settings["channel"] = parameters[i + 1].lower()
-        elif parameter == "-p":
-            if i + 1 >= len(parameters):
-                print("Usage: python test.py -p <port>")
-                sys.exit(1)
-            settings["port"] = parameters[i + 1].lower()
+    def send(self, data):
+        self.s.sendall(data)
 
-    print(settings)
-    url = "http://192.168.0.110:8080"
-    response = requests.put(url, json=settings)
+    def run(self):
+        try:
+            while not self.stopped():
+                data = self.s.recv(BUFFER)  
+                if data:
+                    logg.info("Data received from tunnel ... %s" % data)
+                    self.connection.forward(data)
+                else:
+                    logg.info("No data received")
+                    # sleep for sometime before trying to get data again
+                    time.sleep(1)
+        except Exception as ex:
+            logg.error("Error: %s" % ex)
+        finally: 
+            self.s.close()
 
-    if response.status_code > 299:
-        print(f"Error from server {response.content}")
-        sys.exit(1)
+    def stop(self):
+        self._stop.set()
+        self.s.close()
 
-    data = response.json()
-    if "channel" in data:
-        url += f"/{data['channel']}"
-        createHost(url, settings['type'].lower(), int(settings['port']))
-    elif "type" in data and "port" in data:
-        url += f"/{settings['channel']}"
-        createClient(url, data['type'], int(data['port']))
-
+    def stopped(self):
+        return self._stop.is_set()
 
 if __name__ == "__main__":
-    main()
+    """Parse argument from command line and start tunnel"""
+
+    parser = argparse.ArgumentParser(description='Start Tunnel')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-p', dest='port', help='Port the tunnel connect to')
+    group.add_argument('-c', dest='channel', help='Specify the channel Id')
+
+    args = parser.parse_args()
+
+    if args.channel:
+        with TunnelPersistentConnection(connection_id = args.channel) as persistentConnection:
+            if persistentConnection.create():
+                logg.info("Connection established")
+                with TCPProxyServer(('', persistentConnection.port), TCPProxyHandler, persistentConnection) as server:
+                    logg.info("Waiting for connection...")
+                    server.serve_forever()
+    elif args.port:
+        with TunnelPersistentConnection(port = args.port) as persistentConnection:
+            if persistentConnection.create():
+                persistentConnection.run()
+            
--- server.py
+++ server.py
@@ -1,129 +1,139 @@
-# from js import Response
+#!/usr/bin/env python
 import json
-from urllib.parse import urlparse, parse_qs
+from queue import Queue
+from uuid import uuid4
 from aiohttp import web
 from aiohttp.web import Request, Response
-from queue import Queue
+import argparse
+import logging
 
-# Initialize an in-memory key-value store
-channel_store = {}
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(process)s] [%(levelname)s] %(message)s")
+logg = logging.getLogger(__name__)
 
-class RelayAgent:
-    def __init__(self, host: str, settings: dict):
+class ProxyChannel:
+    def __init__(self, host, settings: dict):
         self.host = host
         self.settings = settings
         self.client = None
         self.host_queue = Queue()
         self.client_queue = Queue()
 
-    def AddClient(self, client: str):
+    def SetClient(self, client):
         if self.client is None:
             self.client = client
             return True
         else:
             return False
 
-    def GetMessage(self, ip: str):
-        if ip == self.host and not self.host_queue.empty():
+    def GetMessage(self, addr):
+        logg.info(f"Getting message from {addr}")
+        if addr == self.host and not self.host_queue.empty():
             return self.host_queue.get()
-        elif ip == self.client and not self.client_queue.empty():
+        elif addr == self.client and not self.client_queue.empty():
             return self.client_queue.get()
         else:
             return None
         
-    def AddMessage(self, ip: str, item):
-        print(f"Adding message from {ip}: {item}")
-        if ip == self.host:
+    def AddMessage(self, addr, item):
+        logg.info(f"Adding message from {addr}: {item}")
+        if self.host == addr and not self.client_queue.full():
             self.client_queue.put(item)
             return True
-        elif ip == self.client:
+        elif self.client == addr and not self.host_queue.full():
             self.host_queue.put(item)
             return True
         else:
             return False
-
-# Define a function that will create a new resource
-async def CreateRlay(request: Request) -> Response:
-    data = await request.json()
-    host_ip = request.remote
-
-    if "channel" in data:
-        channel_id = data["channel"]
-        if channel_id in channel_store:
-            channel = channel_store[channel_id]
-            channel.AddClient(host_ip)
-            return Response(body=json.dumps(channel.settings), status=201)
-        else:
-            return Response(body="Channel not found", status=404)
-    elif "type" in data and "port" in data:
-        channel_id = "12345" # generate a unique channel id
-        if channel_id not in channel_store:
-            channel_store[channel_id] = RelayAgent(host_ip, data)
-            channel = {"channel": channel_id}
-            return Response(body=json.dumps(channel), status=201)
-        else:
-            return Response(body="Client already exists", status=409)
-    else:
-        return Response(body="Invalid request", status=400)
-    # # Save the key_value_map to a database or file (this part is not implemented)
-    # # For demonstration purposes, we will print the key_value_map
-    # for k, v in channel_store.items():
-    #     print (f"Channel: {v} , host: {v.host} <-> clinet: {v.client}")
-
-# Define a function that will relay the request to the appropriate resource based on the key
-async def AddRelayMessage(request: Request) -> Response:
-    channel = request.url.name
-    if channel in channel_store:
-        data = await request.read()
-        channel_store[channel].AddMessage(request.remote, data)
-        # Forward the request to the client_ip
-        # (this part is not implemented, but you can use aiohttp.ClientSession to send a request to the client_ip)
-        return Response(body="Request forwarded to client", status=200)
-    else:
-        return Response(body="Key not found", status=404)
-
-# Define a function that will fetch the value of a resource based on the key
-async def GetRelayMessage(request: Request) -> Response:
-    channel_id = request.url.name
-    if channel_id in channel_store:
-        channel = channel_store[channel_id]
-        # Fetch the value from the client_ip
-        # (this part is not implemented, but you can use aiohttp.ClientSession to send a request to the client_ip)
-        msg = channel.GetMessage(request.remote)
-        return Response(body=msg, status=200)
-    else:
-        return Response(body="Key not found", status=404)
     
-async def on_fetch(request: Request) -> Response:
-    if request.method == "GET":
-        return await GetRelayMessage(request)
-    elif request.method == "POST": # connect to existing resource
-        return await AddRelayMessage(request)
-    elif request.method == "PUT": # create new resource
-        return await CreateRlay(request)
-    elif request.method == "DELETE":
-        return Response(body="Hello world!")
-    else:
-        return Response(body="Access Denied", status=403)
-    # Parse the incoming request URL
-    url = request.url #urlparse(request.url)
-    # Parse the query parameters into a Python dictionary
-    params = url.query #parse_qs(url.query)
 
-    if "name" in params:
-        greeting = "Hello there, {name}".format(name=params["name"])
-        return Response(body=greeting)
+class ProxyRequestHandler():
 
-    if url.path == "/favicon.ico":
-      return Response.new("")
+    channels = {}
+    BUFFER = 1024 * 50 
 
-    return Response(body="Hello world!")
+    def _get_connection_id(self, request: Request):
+        return request.url.name
+
+    def _get_channel(self, request: Request):
+        """get the socket which connects to the target address for this connection"""
+        id = self._get_connection_id(request)
+        return self.channels.get(id, None)
+
+    def _get_request_address(self, request: Request):
+        return request.remote
+        # return request.transport.get_extra_info('peername')
+
+    def _close_channel(self, request: Request):
+        """ close the current socket"""
+        s = self._get_channel(request)
+        if s:
+            del s
+
+    def do_GET(self, request: Request):
+        """GET: Read data from TargetAddress and return to client through http response"""
+        s = self._get_channel(request)
+        if s:
+            data = s.GetMessage(self._get_request_address(request))
+            logg.info('Data received from queue: %s' % data)
+            return Response(status=200, body=data if data else b'')
+        else:
+            logg.error('Connection With ID %s has not been established' % self._get_connection_id(request))
+            return Response(status=400)
+
+    async def do_POST(self, request: Request):
+        """POST: Create TCP Connection to the TargetAddress"""
+        logg.info('Initializing connection')
+        data = await request.json()
+
+        if "channel" in data and data["channel"]:
+            channel_id = data["channel"]
+            if channel_id in self.channels:
+                channel = self.channels[channel_id]
+                channel.SetClient(self._get_request_address(request))
+                return Response(body=json.dumps(channel.settings), status=200)
+            else:
+                return Response(body="Channel not found", status=404)
+        elif "port" in data and data["port"] != -1:
+            channel_id = str(uuid4())
+            if channel_id not in self.channels:
+                data["channel"] = channel_id
+                channel = ProxyChannel(self._get_request_address(request), data)
+                self.channels[channel_id] = channel
+                return Response(body=json.dumps(channel.settings), status=200)
+            else:
+                return Response(body="Client already exists", status=409)
+        else:
+            return Response(body="Invalid request", status=400)
+
+    async def do_PUT(self, request: Request):
+        """Read data from HTTP Request and send to TargetAddress"""
+        s = self._get_channel(request)
+        if not s:
+            logg.error("Connection with id %s doesn't exist" % id)
+            return Response(status=400)
+        data = await request.read()
+        # save the data in the queue
+        logg.info('Saving data .... %s' % data)
+        s.AddMessage(self._get_request_address(request), data)
+        return Response(status=200)
+
+    def do_DELETE(self, request: Request): 
+        id = self._get_connection_id(request) 
+        logg.info('Closing connection with ID %s' % id)
+        if id is not None:
+            del self.channels[id]
+        return Response(status=200)
+        
 
 app = web.Application()
-app.router.add_get('/{channel}', on_fetch)
-app.router.add_post('/{channel}', on_fetch)
-app.router.add_put('/', on_fetch)
-app.router.add_delete('/', on_fetch)
+app.router.add_get('/{id}', lambda request: ProxyRequestHandler().do_GET(request))
+app.router.add_post('/', lambda request: ProxyRequestHandler().do_POST(request))
+app.router.add_put('/{id}', lambda request: ProxyRequestHandler().do_PUT(request))
+app.router.add_delete('/{id}', lambda request: ProxyRequestHandler().do_DELETE(request))
 
-if __name__ == '__main__':
-    web.run_app(app, host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Start Tunnel Server")
+    parser.add_argument("-p", default=9999, dest='port', help='Specify port number server will listen to', type=int)
+    args = parser.parse_args()
+    logg.info("Starting server on port %s" % args.port)
+    web.run_app(app, host='0.0.0.0', port=args.port)
