7946f2d3aca40121517cc6d49eaf3fda45016945
nickdu088 <26584779+nickdu088@users.noreply.github.com>
2025-03-07 16:34:24 +1100

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,6 @@ import socket
 import socketserver
 import time
 import requests
-from uuid import uuid4
 import threading
 import argparse
 import logging
@@ -17,63 +16,56 @@ logg = logging.getLogger(__name__)
 BUFFER = 1024 * 50
 TUNNEL_URL = "http://192.168.0.67:9999"
 
-class TunnelPersistentConnection:
+class TunnelConnection:
   
-    def __init__(self, connection_id : str = None, port : int = -1):
+    def __init__(self, connection_id: str = None, port: int = -1):
         self.id = connection_id
         self.port = port
         self.session = requests.Session()
         self.session.verify = True
-        self.sender = None
         self.lock = threading.Lock()
 
     def get_settings(self):
         return json.dumps({"channel": self.id, "port": self.port})
 
     def get_channel_url(self):
-        if self.id:
-            return TUNNEL_URL + "/" + self.id
-        else:
-            return TUNNEL_URL
+        return f"{TUNNEL_URL}/{self.id}" if self.id else TUNNEL_URL
 
     def create(self):
         logg.info("Creating connection to remote tunnel")
         headers = {"Content-Type": "application/json", "Accept": "text/plain"}
         try:
             data = self.get_settings()
-            logg.info("Creating connection with settings: %s" % data)
-            response = self.session.post(url=TUNNEL_URL, data=self.get_settings(), headers=headers)
+            logg.info("Creating connection with settings: %s", data)
+            response = self.session.post(url=TUNNEL_URL, data=data, headers=headers)
             if response.status_code == 200:
-                settins = response.json()
-                self.id = settins["channel"]
-                self.port = int(settins["port"])
-                logg.info('Successfully create connection : %s' % self.get_settings())
+                settings = response.json()
+                self.id = settings["channel"]
+                self.port = int(settings["port"])
+                logg.info('Successfully created connection: %s', self.get_settings())
                 return True
-            logg.info('Fail to establish connection: status %s because %s' % (response.status_code, response.reason))
+            logg.warning('Failed to establish connection: status %s because %s', response.status_code, response.reason)
             return False 
         except Exception as ex:
-            logg.error("Error Creating Connection: %s" % ex)
+            logg.error("Error Creating Connection: %s", ex)
             return False
 
     def forward(self, data, id):
         with self.lock:
             headers = {"Content-Type": "application/json", "Accept": "text/plain"}
-            if data:
-                data_tosent = {"id": id, "data": base64.b64encode(data).decode()}
-            else:
-                data_tosent = {"id": id}
-            response = self.session.put(url= self.get_channel_url(), data=json.dumps(data_tosent), headers=headers)
-            if (response.status_code == 200):
+            data_to_send = {"id": id, "data": base64.b64encode(data).decode()} if data else {"id": id}
+            response = self.session.put(url=self.get_channel_url(), data=json.dumps(data_to_send), headers=headers)
+            if response.status_code == 200:
                 logg.info("Data forwarded to remote tunnel")
                 return True
             else:
-                logg.info("Fail to forward data to remote tunnel error: %s" % response.reason)
+                logg.warning("Failed to forward data to remote tunnel: %s", response.reason)
                 return False
 
     def receive(self) -> Tuple[str, bytes]:
         with self.lock:
             response = self.session.get(self.get_channel_url())
-            if response.status_code == 200 and response.content != b'':
+            if response.status_code == 200 and response.content:
                 data_received = response.json()
                 logg.info("Data received from remote tunnel")
                 if "id" in data_received and "data" in data_received:
@@ -83,7 +75,7 @@ class TunnelPersistentConnection:
             return None, None
 
     def close(self):
-        logg.info("Close connection to target at remote tunnel")
+        logg.info("Closing connection to target at remote tunnel")
         self.session.delete(self.get_channel_url())
         self.session.close()
 
@@ -99,48 +91,41 @@ class TunnelPersistentConnection:
                         sender.start()
                         senders[id] = sender
                     else:
-                        logg.info("Sender already exist")
-                        senders.get(id).stop()
-                        senders.get(id).join()
+                        logg.info("Sender already exists")
+                        senders[id].stop()
+                        senders[id].join()
                         del senders[id]
                 if id and data:
-                    logg.info("Data received from tunnel: %s" % data)
+                    logg.info("Data received from tunnel: %s", data)
                     if id not in senders:
                         logg.error("Sender not found")
-                    senders.get(id).send(data)
+                    senders[id].send(data)
                 else:
                     time.sleep(1)
             except Exception as ex:
-                logg.error("Error Receiving Data: %s" % ex)
+                logg.error("Error Receiving Data: %s", ex)
                 break
 
 class ReceiveThread(threading.Thread):
 
-    """
-    Thread to receive data from remote host
-    """
-
     def __init__(self, connection, client):
-        threading.Thread.__init__(self, name="Receive-Thread")
+        super().__init__(name="Receive-Thread")
         self.client = client
         self.conn = connection
-        self.id = id
         self._stop = threading.Event()
 
     def run(self):
         while not self.stopped():
             try:
-                logg.info("Retreiving data from remote tunneld")
+                logg.info("Retrieving data from remote tunnel")
                 id, data = self.conn.receive()
                 if data:
-                    logg.info("Data received ... %s" % data)
+                    logg.info("Data received: %s", data)
                     self.client.send(id, data)
                 else:
-                    # logg.info("No data received")
-                    # sleep for sometime before trying to get data again
                     time.sleep(1)
             except Exception as ex:
-                logg.error("Error: %s" % ex)
+                logg.error("Error: %s", ex)
                 break
 
     def stop(self):
@@ -152,20 +137,16 @@ class ReceiveThread(threading.Thread):
 class TCPProxyHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
-        # self.rfile is a file-like object created by the handler.
-        # We can now use e.g. readline() instead of raw recv() calls.
-        # We limit ourselves to 10000 bytes to avoid abuse by the sender.
         while True:
             try:
                 data = self.request.recv(BUFFER)
-                logg.info("Getting data from client to send")
-                if data == b'':
+                if not data:
                     logg.info("Client's socket connection broken")
                     break
-                logg.info("Sending data ... %s " % data)
+                logg.info("Sending data: %s", data)
                 self.server.forward_request(data, str(self.request.__hash__()))
             except Exception as ex:
-                logg.error("Error: %s" % ex)
+                logg.error("Error: %s", ex)
                 break
 
 class TCPProxyServer(socketserver.ThreadingTCPServer):
@@ -178,7 +159,6 @@ class TCPProxyServer(socketserver.ThreadingTCPServer):
     def process_request(self, request, client_address):
         id = str(request.__hash__())
         self.sockets[id] = request
-        # new connection to remote server
         self.tunnelConnection.forward(None, id)
         return super().process_request(request, client_address)
 
@@ -192,7 +172,6 @@ class TCPProxyServer(socketserver.ThreadingTCPServer):
     def shutdown_request(self, request):
         id = str(request.__hash__())
         del self.sockets[id]
-        # close connection to remote server
         self.tunnelConnection.forward(None, id)
         return super().shutdown_request(request)
 
@@ -209,14 +188,11 @@ class TCPProxyServer(socketserver.ThreadingTCPServer):
 
 class TCPProxyClient(threading.Thread):
     def __init__(self, remote_addr, id, connection):
-        threading.Thread.__init__(self, name="Client-Thread")
+        super().__init__(name="Client-Thread")
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection = connection
         self.id = id
-        # open socket connection to remote server
         self.s.connect_ex((remote_addr['host'], int(remote_addr['port'])))
-        # use non-blocking socket
-        # self.s.setblocking(0)
         self._stop = threading.Event()
     
     def send(self, data):
@@ -227,14 +203,12 @@ class TCPProxyClient(threading.Thread):
             while not self.stopped():
                 data = self.s.recv(BUFFER)  
                 if data:
-                    logg.info("Data received from tunnel ... %s" % data)
+                    logg.info("Data received from tunnel: %s", data)
                     self.connection.forward(data, self.id)
                 else:
-                    logg.info("No data received")
-                    # sleep for sometime before trying to get data again
                     time.sleep(1)
         except Exception as ex:
-            logg.error("Error: %s" % ex)
+            logg.error("Error: %s", ex)
         finally: 
             self.s.close()
             self.connection.forward(None, self.id)
@@ -250,8 +224,6 @@ class TCPProxyClient(threading.Thread):
         self.s.close()
 
 if __name__ == "__main__":
-    """Parse argument from command line and start tunnel"""
-
     parser = argparse.ArgumentParser(description='Start Tunnel Client')
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-r', dest='remote', help='Specify the host and port of the remote server to tunnel to (e.g. localhost:22)')
@@ -260,7 +232,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.channel:
-        tunnelConnection = TunnelPersistentConnection(connection_id = args.channel)
+        tunnelConnection = TunnelConnection(connection_id=args.channel)
         if tunnelConnection.create():
             logg.info("Connection established")
             with TCPProxyServer(('', tunnelConnection.port), TCPProxyHandler, tunnelConnection) as server:
@@ -268,8 +240,7 @@ if __name__ == "__main__":
                 server.serve_forever()
     elif args.remote:
         remote_addr = {"host": args.remote.split(":")[0], "port": args.remote.split(":")[1]}
-        tunnelConnection = TunnelPersistentConnection(port = remote_addr['port'])
+        tunnelConnection = TunnelConnection(port=remote_addr['port'])
         if tunnelConnection.create():
             tunnelConnection.run(remote_addr)
             tunnelConnection.close()
-            
