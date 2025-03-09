505bdbedf5bb2b92605c26bce4cab4a457f2e12c
nickdu088 <26584779+nickdu088@users.noreply.github.com>
2025-03-09 21:30:03 +1100

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ class SessionTunnelInfoStorage implements TunnelInfoStorage {
     }
 
     public function getTunnelInfo($channel_id) {
-        return isset($_SESSION[$channel_id]) ? $_SESSION[$channel_id] : null;
+        return $_SESSION[$channel_id] ?? null;
     }
 
     public function deleteTunnelInfo($channel_id) {
@@ -40,6 +40,7 @@ class ProxyTunnel {
         $this->host = $host;
         $this->settings = $settings;
         $this->client = null;
+        $this->date_created = date('Y-m-d H:i:s');
         $this->host_queue = new SplQueue();
         $this->client_queue = new SplQueue();
     }
@@ -76,16 +77,40 @@ class ProxyTunnel {
         }
         return false;
     }
+
+    public function __toString() {
+        return json_encode([
+            'host' => $this->host,
+            'settings' => $this->settings,
+            'client' => $this->client,
+            'date_created' => $this->date_created
+        ]);
+    }
 }
 
+// function cleanUpSessions() {
+//     global $tunnelStorage;
+//     foreach ($_SESSION as $channel_id => $channel) {
+//         if ($channel instanceof ProxyTunnel) {
+//             // Check if the tunnel is older than 1 hour
+//             $date_created = new DateTime($channel->date_created);
+//             $now = new DateTime();
+//             $interval = $now->diff($date_created);
+//             if ($interval->h >= 1) {
+//                 $tunnelStorage->deleteTunnelInfo($channel_id);
+//             }
+//         }
+//     }
+// }
+
+// // Run the cleanup task every hour
+// if (!isset($_SESSION['last_cleanup']) || (time() - $_SESSION['last_cleanup']) > 3600) {
+//     cleanUpSessions();
+//     $_SESSION['last_cleanup'] = time();
+// }
+
 function getClientIp() {
-    if (isset($_SERVER['HTTP_CLIENT_IP'])) {
-        return $_SERVER['HTTP_CLIENT_IP'];
-    } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        return $_SERVER['HTTP_X_FORWARDED_FOR'];
-    } else {
-        return $_SERVER['REMOTE_ADDR'];
-    }
+    return $_SERVER['HTTP_CLIENT_IP'] ?? $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'];
 }
 
 function handleRequest() {
@@ -130,15 +155,13 @@ function handleGet() {
         echo $message;
         return;
     }
-
-    http_response_code(404);
+    echo "You are not authorized to access this.";
 }
 
 function uuidv4() {
     $data = random_bytes(16);
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
-    
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);   
     return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
 
@@ -194,7 +217,9 @@ function handleDelete() {
 
 function handleOptions() {
     foreach ($_SESSION as $key => $val) {
-        echo $key . "\r\n";
+        if ($val instanceof ProxyTunnel) {
+            echo $key . ": " . $val->date_created . "\r\n";
+        }
     }
 }
 
