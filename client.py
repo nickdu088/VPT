0da153a9ec25da5979e03962369d1e35a8ab3210
nickdu088 <26584779+nickdu088@users.noreply.github.com>
2025-05-09 17:00:33 +1000

    Update client.py

--- client.py
+++ client.py
@@ -8,8 +8,6 @@ import base64
 import lzma
 from typing import Tuple, AsyncGenerator
 
-import urllib3
-
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(process)s] [%(levelname)s] %(message)s")
 logg = logging.getLogger(__name__)
 
@@ -21,8 +19,8 @@ class TunnelConnection:
     def __init__(self, connection_id: str = None, port: int = -1):
         self.id = connection_id
         self.port = port
-        self.session = aiohttp.ClientSession()
-        urllib3.disable_warnings()
+        timeout = aiohttp.ClientTimeout(total=None)  # Disable timeout
+        self.session = aiohttp.ClientSession(timeout=timeout)
 
     def get_settings(self):
         return {"channel": self.id, "port": self.port}
@@ -46,11 +44,7 @@ class TunnelConnection:
         return False
 
     async def forward(self, data, id):
-        if data:
-            data = lzma.compress(data)
-            data_to_send = {"id": id, "data": base64.b64encode(data).decode()}
-        else:
-            data_to_send = {"id": id}
+        data_to_send = {"id": id, "data": base64.b64encode(lzma.compress(data)).decode()} if data else {"id": id}
         
         async with self.session.put(url=self.get_channel_url(), json=data_to_send) as response:
             if response.status == 200:
@@ -64,9 +58,9 @@ class TunnelConnection:
         async with self.session.get(self.get_channel_url()) as response:
             if response.status == 200:
                 buffer = b''
-                async for line, end_of_http in response.content.iter_chunks():
+                async for line, end_of_line in response.content.iter_chunks():
                     buffer += line
-                    if end_of_http and buffer != b'':
+                    if end_of_line and buffer != b'\n':
                         logg.info("Data << from remote tunnel")
                         try:
                             data_received = json.loads(buffer)
@@ -101,27 +95,10 @@ class TunnelConnection:
                     senders[id] = (sender, task)
                 else:
                     logg.info("Closing sender %s", id)
-                    senders[id][0].stop()
                     senders[id][1].cancel()
             elif id and data and id in senders:
                 await senders[id][0].send(data)
         await self.close()
-
-class ReceiveTask:
-
-    def __init__(self, connection, client):
-        self.client = client
-        self.conn = connection
-
-    async def run(self):
-        async for id, data in self.conn.receive():
-            if data:
-                await self.client.send(id, data)
-            else:
-                break
-        await self.client.close()
-        await self.client.forward(None, id)
-
 
 class TCPProxyServer:
     def __init__(self, port, connection):
@@ -129,11 +106,24 @@ class TCPProxyServer:
         self.writer = None
         self.connection = connection
         self.port = port
-        self.sockets = {}
+        self.senders = {}
         self.receiver = None
 
+    async def receive(self):
+        try:
+            async for id, data in self.connection.receive():
+                if not data:
+                    self.senders[id][1].cancel()
+                    await self.senders[id][1]
+                    del self.senders[id]
+                await self.send(id, data)
+        except asyncio.CancelledError:
+            pass
+        except Exception as e:
+            logg.error("Error in receive: %s", e)
+
     async def start(self):
-        self.receiver = asyncio.create_task(ReceiveTask(self.connection, self).run())
+        self.receiver = asyncio.create_task(self.receive())
         async with await asyncio.start_server(self.handle_client, port=self.port) as server:
             await server.serve_forever()
             self.receiver.cancel()
@@ -142,27 +132,30 @@ class TCPProxyServer:
     async def handle_client(self, reader, writer):
         id = str(writer.__hash__())
         logg.info("New client connected: %s", id)
-        await self.connection.forward(None, id)
-        asyncio.create_task(self.handle_connection(reader, writer))
-        self.sockets[id] = writer
+        sender = asyncio.create_task(self.handle_connection(reader, writer))
+        self.senders[id] = (writer, sender)
     
     async def handle_connection(self, reader, writer):
-        while True:
-            try:
+        id = str(writer.__hash__())
+        await self.connection.forward(None, id)
+        try:
+            while True:
                 data = await reader.read(BUFFER)
                 if not data:
                     logg.info("Client's socket connection broken")
                     break
-                await self.connection.forward(data, str(writer.__hash__()))
-            except Exception as e:
-                logg.error("Unexpected error: %s", e)
-                break
-        await self.connection.forward(None, str(writer.__hash__()))
+                await self.connection.forward(data, id)
+        except Exception as e:
+            logg.error("Error: %s", e)
+        except asyncio.CancelledError:
+            pass
+        finally:
+            await self.connection.forward(None, id)
     
     async def send(self, id, data):
-        if id in self.sockets:
-            self.sockets[id].write(data)
-            await self.sockets[id].drain()
+        if id in self.senders:
+            self.senders[id][0].write(data)
+            await self.senders[id][0].drain()
 
 class TCPProxyClient:
     def __init__(self, remote_addr, id, connection):
@@ -171,7 +164,6 @@ class TCPProxyClient:
         self.connection = connection
         self.id = id
         self.remote_addr = remote_addr
-        self._stop = asyncio.Event()
 
     async def connect(self):
         try:
@@ -189,27 +181,18 @@ class TCPProxyClient:
 
     async def run(self):
         try:
-            while not self.stopped():
+            while True:
                 data = await self.reader.read(BUFFER)
-                if data == b'':
+                if not data:
                     logg.info("Connection closed by remote server")
                     break
-                if data:
-                    await self.connection.forward(data, self.id)
+                await self.connection.forward(data, self.id)
+        except asyncio.CancelledError:
+            pass
         except Exception as ex:
             logg.error("Error: %s", ex)
         finally:
-            await self.close()
             await self.connection.forward(None, self.id)
-
-    def stop(self):
-        self._stop.set()
-
-    def stopped(self):
-        return self._stop.is_set()
-
-    async def close(self):
-        if self.writer:
             self.writer.close()
             await self.writer.wait_closed()
 
